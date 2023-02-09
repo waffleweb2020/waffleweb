4a695b33b96f6dadcf0baa35745d3161d6503532
waffleweb <waffleweb.it@gmail.com>
2023-02-09 16:36:43 +0600

    commit by 9.02

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,17 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start'
+      args: 'start',
+      env: {
+        NODE_ENV: 'development',
+        port:8080,
+        sitename:'React.js Website'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        port:8080,
+        sitename:'React.js Website'
+      }
     }
   ]
 }
