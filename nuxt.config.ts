2be581eb2b93dd5feb67b3d3a4f9201d50ac51eb
waffleweb <waffleweb.it@gmail.com>
2023-02-13 17:50:34 +0600

    13.02

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,9 +16,19 @@ export default defineNuxtConfig({
             title: 'WaffleWeb - разработка сайтов',
             meta: [
                 // <meta name="description" content="My amazing site">
-                { name: 'description', content: 'My amazing site.' }
+                { name: 'description', content: 'У нас вы сможете заказать разработку веб сайта или телеграм-бота. Профессиональная разработка сайтов в Омске. ☎ +7 913 674 60 18' },
+                { name: 'robots', content: 'content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"'},
+                { property: "og:locale", content: 'ru_RU'},
+                { property: "og:type", content: 'website'},
+                { property: "og:title", content: 'Создание сайта в Омске | 👍 Компания разработки сайтов Waffle web'},
+                { property: "og:url", content: 'https://www.waffle-web.ru'},
+                { property: "og:site_name", content: 'Компания разработки сайтов Waffle web'},
+                { property: "og:site_name", content: 'Компания разработки сайтов Waffle web'},
+                { property: "og:description", content: 'У нас вы сможете заказать разработку веб сайта или телеграм-бота. Профессиональная разработка сайтов в Омске. ☎ +7 913 674 60 18'},
             ],
             link: [
+                { rel: 'canonical', href: 'https://www.waffle-web.ru'},
+                { rel: 'alternate', type:'application/rss+xml', title: 'Компания разработки сайтов Waffle web', href: 'https://www.waffle-web.ru'},
                 {rel: "preconnect", href: "https://fonts.googleapis.com"},
                 { rel : "preconnect", href: "https://fonts.googleapis.com"},
                 { href: "https://fonts.googleapis.com/css2?family=Ubuntu&display=swap", rel: "stylesheet" },
